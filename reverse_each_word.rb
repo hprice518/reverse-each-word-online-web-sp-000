3c0841632d7fa7d762a74f4666577970ada2d1c8
Harrison Price <hprice518@gmail.com>
2021-03-13 20:39:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 #"Hello there, and how are you?"
 
 def reverse_each_word(sentence)
-  sentence.split.each {|word| word.reverse}.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
